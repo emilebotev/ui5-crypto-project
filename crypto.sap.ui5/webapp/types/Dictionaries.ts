6732d961863b84007940fd3ac6f1ff2c3f44503d
Emil Botev <emil_botev@epam.com>
2025-07-25 11:36:40 +0000

    feat: created enum dictionaries and types for the new data for better typing and autocomplete assistance

---
+++ crypto.sap.ui5/webapp/types/Dictionaries.ts
@@ -0,0 +1,25 @@
+import TopMarketCap from "../controller/TopMarketCap.controller";
+
+export enum ModelNamesDictionary {
+  cryptoModel = "cryptoModel",
+  topMarketCap = "topMarketCap",
+  cryptoDetail = "cryptoDetail"
+}
+
+// Crypto Model Properties
+export enum CryptoModelPropsDictionary {
+  supportedCurrencies = "/supportedCurrencies",
+  selectedCurrency = "/selectedCurrency",
+}
+
+// Top Market Cap Model properties
+export enum TMCModelPropsDictionary {
+  page = "/page",
+  crypto = "/crypto",
+}
+
+export enum CryptoDetailModelPropsDictionary {
+  cryptoDetail = '/cryptoDetail',
+  selectedDays = "/selectedDays",
+  priceAxisRange = "/priceAxisRange",
+}
